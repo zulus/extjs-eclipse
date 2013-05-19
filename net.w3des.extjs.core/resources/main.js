2eb6fda62e1cce377f9a728d7389928ae4561901
zulus <zulus@w3des.net>
2013-05-19 19:42:01 +0200

    Basic Variable initializer

---
+++ net.w3des.extjs.core/resources/main.js
@@ -0,0 +1,41 @@
+/**
+ * Empty class for inference engine. Please ignore it!
+ * 
+ * @class Ext
+ * @private
+ */
+var Ext = function() {
+};
+Ext.dom = {};
+Ext.core = {};
+Ext.DomQuery = {};
+Ext.app = {};
+Ext.button = {};
+Ext.char = {};
+Ext.container = {};
+Ext.ux = {};
+Ext.data = {};
+Ext.dd = {};
+Ext.direct = {};
+Ext.dom = {};
+Ext.draw = {};
+Ext.enums = {};
+Ext.flash = {};
+Ext.form = {};
+Ext.fx = {};
+Ext.grid = {};
+Ext.layout = {};
+Ext.menu = {};
+Ext.panel = {};
+Ext.picker = {};
+Ext.resizer = {};
+Ext.selection = {};
+Ext.slider = {};
+Ext.state = {};
+Ext.tab = {};
+Ext.tip = {};
+Ext.toolbar = {};
+Ext.tree = {};
+Ext.util = {};
+Ext.view = {};
+Ext.window = {};
---
+++ net.w3des.extjs.core/src/net/w3des/extjs/core/infer/VariableInitializer.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package net.w3des.extjs.core.infer;
+
+import net.w3des.extjs.core.ExtJSCore;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.wst.jsdt.core.JavaScriptCore;
+import org.eclipse.wst.jsdt.core.JavaScriptModelException;
+import org.eclipse.wst.jsdt.core.JsGlobalScopeVariableInitializer;
+
+/**
+ * @author Dawid zulus Pakula <zulus@w3des.net>
+ * 
+ */
+public class VariableInitializer extends JsGlobalScopeVariableInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.wst.jsdt.core.JsGlobalScopeVariableInitializer#initialize
+	 * (java.lang.String)
+	 */
+	@Override
+	public void initialize(String variable) {
+		if (!variable.equals("Ext")) { //$NON-NLS-1$
+			try {
+				JavaScriptCore.setIncludepathVariable("Ext",
+						new Path(ExtJSCore.getDefault().getBundle().getEntry("resources/main.js").toString()),
+						new NullProgressMonitor());
+			} catch (JavaScriptModelException e) {
+				ExtJSCore.error(e);
+			}
+		}
+	}
+
+}
