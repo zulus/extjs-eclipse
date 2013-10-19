1e2b0aaf2a5977a4f463dee9791c7470bf71e5e7
Dawid Pakuła <zulus@w3des.net>
2013-10-19 15:51:28 +0200

    fix #4

---
+++ net.w3des.extjs.core/src/net/w3des/extjs/internal/core/validation/TrailingCommaValidation.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright (c) 2013 w3des.net and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *      w3des.net - initial API and implementation
+ ******************************************************************************/
+package net.w3des.extjs.internal.core.validation;
+
+import java.util.HashSet;
+
+import net.w3des.extjs.internal.core.ExtJSCore;
+import net.w3des.extjs.internal.core.validation.problem.ProblemIdentifier;
+import net.w3des.extjs.internal.core.validation.problem.ProblemSeverity;
+import net.w3des.extjs.internal.core.validation.problem.ValidationProblem;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.wst.jsdt.core.IBuffer;
+import org.eclipse.wst.jsdt.core.IJavaScriptProject;
+import org.eclipse.wst.jsdt.core.JavaScriptModelException;
+import org.eclipse.wst.jsdt.core.compiler.BuildContext;
+import org.eclipse.wst.jsdt.core.compiler.CategorizedProblem;
+import org.eclipse.wst.jsdt.core.compiler.ReconcileContext;
+import org.eclipse.wst.jsdt.core.compiler.ValidationParticipant;
+
+/**
+ * TODO Configurable severity
+ * 
+ * @author Dawid zulus Pakula <zulus@w3des.net>
+ */
+public class TrailingCommaValidation extends ValidationParticipant {
+
+	@Override
+	public boolean isActive(IJavaScriptProject project) {
+		return ExtJSCore.getProjectManager().isExtJSProject(project.getProject());
+	}
+
+	@Override
+	public void reconcile(ReconcileContext context) {
+		super.reconcile(context);
+		try {
+			IResource resource = context.getWorkingCopy().getCorrespondingResource();
+			if (resource instanceof IFile) {
+				IBuffer buffer = context.getWorkingCopy().getBuffer();
+				context.putProblems(ValidationProblem.MARKER_TYPE, validate(buffer.getCharacters(), resource.getFullPath().toString().toCharArray()));
+			}
+		} catch (JavaScriptModelException e) {
+		}
+	}
+
+	@Override
+	public void buildStarting(BuildContext[] files, boolean isBatch) {
+		super.buildStarting(files, isBatch);
+		if (files == null) {
+			return;
+		}
+
+		for (BuildContext context : files) {
+			context.recordNewProblems(this.validate(context.getContents(), context.getFile().getName().toCharArray()));
+		}
+	}
+
+	private CategorizedProblem[] validate(char[] contents, char[] fileName) {
+		HashSet<CategorizedProblem> problems = new HashSet<CategorizedProblem>();
+		int test = 0;
+		int line = 1;
+		for(int pos = 0; pos < contents.length; pos++) {
+			final char current  = contents[pos];
+			if (current == '\n') { //$NON-NLS-1$
+				line++;
+			}
+			if (Character.isWhitespace(current)) {
+				continue;
+			}
+			
+			if (current == ',') {
+				test = pos;
+				continue;
+			}
+			if (test == 0) {
+				continue;
+			}
+			
+			if (current == ']' || current == '}') {
+				problems.add(new ValidationProblem(ProblemIdentifier.TRAILING_COMMA, ValidationProblem.CAT_UNNECESSARY_CODE, ProblemSeverity.WARNING, "Trailing comma is not legal in an ECMA-262", null, fileName, test, test+1, line));
+			}
+			test = 0;
+		}
+		
+		return problems.toArray(new CategorizedProblem[problems.size()]);
+	}
+
+	@Override
+	public void cleanStarting(IJavaScriptProject project) {
+		super.cleanStarting(project);
+	}
+}
---
+++ net.w3des.extjs.core/src/net/w3des/extjs/internal/core/validation/problem/ValidationProblem.java
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ * Copyright (c) 2013 w3des.net and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *      w3des.net - initial API and implementation
+ ******************************************************************************/
+package net.w3des.extjs.internal.core.validation.problem;
+
+import org.eclipse.wst.jsdt.core.compiler.CategorizedProblem;
+
+public class ValidationProblem extends CategorizedProblem {
+
+	public static String MARKER_TYPE = "net.w3des.extjs.validation.problem";
+
+	private String[] arguments;
+	private String message;
+	private int id;
+	private int category = CAT_UNSPECIFIED;
+	private char[] fileName;
+	private int sourceStart;
+	private int sourceEnd;
+	private int sourceLine;
+	private ProblemSeverity severity;
+	
+	public ValidationProblem(int id, int category, ProblemSeverity severity, String message, String[] arguments, char[] fileName, int sourceStart, int sourceEnd, int lineNumber) {
+		this.id = id;
+		this.category = category;
+		this.message = message;
+		this.arguments = arguments;
+		this.fileName = fileName;
+		this.sourceStart = sourceStart;
+		this.sourceEnd = sourceEnd;
+		this.sourceLine = lineNumber;
+		this.severity = severity;
+	}
+	
+	public ProblemSeverity getSeverity() {
+		return severity;
+	}
+
+	@Override
+	public String[] getArguments() {
+		return arguments == null ? new String[0] : arguments;
+	}
+
+	@Override
+	public int getID() {
+		return id;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public char[] getOriginatingFileName() {
+		return fileName;
+	}
+
+	@Override
+	public int getSourceEnd() {
+		return sourceEnd;
+	}
+
+	@Override
+	public int getSourceLineNumber() {
+		return sourceLine;
+	}
+
+	@Override
+	public int getSourceStart() {
+		return sourceStart;
+	}
+
+	@Override
+	public boolean isError() {
+		return severity == ProblemSeverity.ERROR;
+	}
+
+	@Override
+	public boolean isWarning() {
+		return severity == ProblemSeverity.WARNING;
+	}
+
+	@Override
+	public void setSourceEnd(int sourceEnd) {
+		this.sourceEnd = sourceEnd;
+	}
+
+	@Override
+	public void setSourceLineNumber(int lineNumber) {
+		this.sourceLine = lineNumber;
+	}
+
+	@Override
+	public void setSourceStart(int sourceStart) {
+		this.sourceStart = sourceStart;
+	}
+
+	@Override
+	public int getCategoryID() {
+		return category;
+	}
+
+	@Override
+	public String getMarkerType() {
+		return MARKER_TYPE;
+	}
+
+}
