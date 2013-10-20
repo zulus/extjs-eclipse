4a82ad290a31546599c01580b5d61b56c611a4f0
Dawid Pakuła <zulus@w3des.net>
2013-10-20 13:45:08 +0200

    Better trailing comma parser #4

--- net.w3des.extjs.core/src/net/w3des/extjs/internal/core/validation/TrailingCommaValidation.java
+++ net.w3des.extjs.core/src/net/w3des/extjs/internal/core/validation/TrailingCommaValidation.java
@@ -76,29 +76,96 @@ public class TrailingCommaValidation extends ValidationParticipant {
 		HashSet<CategorizedProblem> problems = new HashSet<CategorizedProblem>();
 		int test = 0;
 		int line = 1;
-		for(int pos = 0; pos < contents.length; pos++) {
-			final char current  = contents[pos];
+		char before = 0;
+		boolean comment = false;
+		boolean lineComment = false;
+		boolean string = false;
+		boolean dbString = false;
+		boolean regex = false;
+		boolean charSequence = false;
+		for (int pos = 0; pos < contents.length; pos++) {
+			final char current = contents[pos];
 			if (current == '\n') { //$NON-NLS-1$
 				line++;
-			}
-			if (Character.isWhitespace(current)) {
+				lineComment = false;
+				before = 0;
+				continue;
+			} else if (lineComment || Character.isWhitespace(current)) {
+				continue;
+			} else if (before == '\\') {
+				before = 0;
 				continue;
 			}
-			
-			if (current == ',') {
-				test = pos;
-				continue;
+			switch(current) {
+			case '[':
+				if (regex) {
+					charSequence = true;
+				}
+				break;
+			case ']':
+				charSequence = false;
+				break;
+			case '*':
+				if (before == '/' && !dbString && !string && !regex) {
+					comment = true;
+				}
+				break;
+			case '\'':
+				if (!regex && !dbString && !comment && !string) {
+					string = true;
+				} else if (string) {
+					string = false;
+				}
+				break;
+			case '"':
+				if (!regex && !dbString && !comment && !string) {
+					dbString = true;
+				} else if (dbString) {
+					dbString = false;
+				}
+				break;
+			case '/':
+				switch (before) {
+				case '/':
+					if (!lineComment && !regex && !dbString && !string) {
+						lineComment = true;
+						break;
+					}
+				case '*':
+					if (comment) {
+						comment = false;
+						break;
+					}
+				default:
+					if (!comment && !lineComment && !string && !dbString) {
+						if (!regex) {
+							regex = true;
+						} else if (regex && !charSequence) {
+							regex = false;
+						}
+					}
+				}
+				break;
+			case ',':
+				if (!comment && !lineComment && !regex && !dbString && !string) {
+					test = pos;
+					before = current;
+					continue;
+				}
 			}
+			before = current;
+
 			if (test == 0) {
 				continue;
 			}
-			
-			if (current == ']' || current == '}') {
-				problems.add(new ValidationProblem(ProblemIdentifier.TRAILING_COMMA, ValidationProblem.CAT_UNNECESSARY_CODE, ProblemSeverity.WARNING, "Trailing comma is not legal in an ECMA-262", null, fileName, test, test+1, line));
+
+			if (!comment && !lineComment && !regex && !dbString && !string && (current == ']' || current == '}')) {
+				problems.add(new ValidationProblem(ProblemIdentifier.TRAILING_COMMA, ValidationProblem.CAT_UNNECESSARY_CODE, ProblemSeverity.WARNING,
+						"Trailing comma is not legal in an ECMA-262", null, fileName, test, test + 1, line));
 			}
 			test = 0;
 		}
-		
+
 		return problems.toArray(new CategorizedProblem[problems.size()]);
 	}
 
