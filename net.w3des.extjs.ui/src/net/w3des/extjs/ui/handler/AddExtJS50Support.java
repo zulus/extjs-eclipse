11b60ebd18e68446c2e7a42dee215404d9690c81
Martin Eisengardt <mep_eisen@web.de>
2014-06-18 07:24:53 +0200

    extjs 5.0 faceted version

---
+++ net.w3des.extjs.ui/src/net/w3des/extjs/ui/handler/AddExtJS50Support.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2013 w3des.net and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *      w3des.net - initial API and implementation
+ ******************************************************************************/
+package net.w3des.extjs.ui.handler;
+
+import javax.inject.Named;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+public class AddExtJS50Support extends AddExtJSSupport {
+
+    @Execute
+    public static void execute(@Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection selection) throws ExecutionException {
+        execute(selection, "5.0");
+    }
+
+}
