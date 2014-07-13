157f27fbe0e5db43439a2178d1447ab47933458c
Dawid Pakuła <zulus@w3des.net>
2014-07-13 20:02:22 +0200

    Fix NPE
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

--- net.w3des.extjs.core/src/net/w3des/extjs/core/builder/ExtJSBuilder.java
+++ net.w3des.extjs.core/src/net/w3des/extjs/core/builder/ExtJSBuilder.java
@@ -184,7 +184,7 @@ public class ExtJSBuilder extends IncrementalProjectBuilder {
 		final IJavaScriptProject jsProject = JavaScriptCore.create(getProject());
 		boolean found = false;
 		for (final IIncludePathEntry entry : jsProject.getRawIncludepath()) {
-			if (entry.getEntryKind() == IIncludePathEntry.CPE_SOURCE) {
+			if (entry.getEntryKind() == IIncludePathEntry.CPE_SOURCE && entry.getPath() != null) {
 				if (entry.getPath().isPrefixOf(source.getFullPath())) {
 					found = true;
 				}
